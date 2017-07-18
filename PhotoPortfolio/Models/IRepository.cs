e8d055cfac729288d2cf57af7c03820b60c741b8
D3n3tsu <d3n3tsu@gmail.com>
2017-07-18 07:44:11 +0300

    WrongCommit. It should be in other branch but shit happens...

---
+++ PhotoPortfolio/Models/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoPortfolio.Models
+{
+    public interface IRepository
+    {
+        void CreatePhotographer(string name, DateTime birthDate);
+        Photographer GetPhotographer(int id);
+        void UpdatePhotographer(Photographer photographer);
+        void DeletePhotographer(int id);
+    }
+}
--- PhotoPortfolio/wwwroot/PortfolioApp/infoController.js
+++ PhotoPortfolio/wwwroot/PortfolioApp/infoController.js
@@ -6,12 +6,44 @@
         .controller('infoController', infoController);
     
 
-    function infoController() {
+    function infoController($http) {
         var vm = this;
         vm.title = 'Info';
+        vm.url = '';
 
         activate();
 
-        function activate() { }
+        function activate() {
+            vm.getData();
+            vm.createPhotographer = createPhGrapher;
+        }
+
+        function createPhGrapher() {
+            
+            
+            $http.post(vm.url + '/' + vm.newName + '/' + vm.newBirthDate)
+                .success(function (responce) {
+                    //success
+                })
+                .error(function (responce) {
+                    //failure
+                })
+                .finally(function () {
+                    //finally
+                });
+        }
+
+        function getData() {
+            $http.get(vm.url)
+                .success(function (responce) {
+                    //success
+                })
+                .error(function (responce) {
+                    //failure
+                })
+                .finally(function () {
+                    //finally
+                });
+        }
     }
 })();
