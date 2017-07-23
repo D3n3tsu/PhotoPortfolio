350f124583fd7ad4ac6014a300e3a04760ac0f74
D3n3tsu <d3n3tsu@gmail.com>
2017-07-23 18:17:51 +0300

    Added class that checks db creation when application starts up

---
+++ PhotoPortfolio/Infrastructure/DatabaseCreation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoPortfolio.Infrastructure
+{
+    public class DatabaseCreation
+    {
+        public void EnsureTableCreation()
+        {
+            using(SqliteConnection con = new SqliteConnection(
+                Startup.Configuration["ConnectionStrings:SQLiteConnection"]))
+            {
+                using(SqliteCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "CREATE table IF NOT EXISTS photographers "
+                        + "(id INTEGER PRIMARY KEY NOT NULL, "
+                        + "name TEXT NOT NULL, "
+                        + "birth_date TEXT NOT NULL)";
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch(SqliteException ex)
+                    {
+                        throw new Exception("Problem while table creation " + ex);
+                    }
+                }
+            }
+        }
+    }
+}
